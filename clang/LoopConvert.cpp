6486c2d249e7573037807c8ef8335d6055d85d7e
Hebi Li <lihebi.com@gmail.com>
2017-02-21 17:13:35 -0600

    finish cmake compilation config

--- clang/LoopConvert.cpp
+++ clang/LoopConvert.cpp
@@ -4,20 +4,44 @@
 
 #include "llvm/Support/CommandLine.h"
 
+#include "clang/AST/ASTConsumer.h"
+#include "clang/CodeGen/ObjectFilePCHContainerOperations.h"
+#include "clang/Driver/Options.h"
+#include "clang/Frontend/ASTConsumers.h"
+#include "clang/Frontend/CompilerInstance.h"
+#include "clang/Rewrite/Frontend/FixItRewriter.h"
+#include "clang/Rewrite/Frontend/FrontendActions.h"
+#include "clang/StaticAnalyzer/Frontend/FrontendActions.h"
+#include "clang/Tooling/CommonOptionsParser.h"
+#include "clang/Tooling/Tooling.h"
+#include "llvm/ADT/STLExtras.h"
+#include "llvm/Option/OptTable.h"
+#include "llvm/Support/Path.h"
+#include "llvm/Support/Signals.h"
+#include "llvm/Support/TargetSelect.h"
+
+
 using namespace clang::tooling;
 using namespace llvm;
 
 // Apply a custom category to all command-line options so that they are the
 // only ones displayed.
 static llvm::cl::OptionCategory MyToolCategory("my-tool options");
+static cl::extrahelp CommonHelp(CommonOptionsParser::HelpMessage);
 
 // CommonOptionsParser declares HelpMessage with a description of the common
 // command-line options related to the compilation database and input files.
 // It's nice to have this help message in all tools.
-static cl::extrahelp CommonHelp(CommonOptionsParser::HelpMessage);
+// static cl::extrahelp CommonHelp(CommonOptionsParser::HelpMessage);
 
 // A help message for this specific tool can be added afterwards.
-static cl::extrahelp MoreHelp("\nMore help text...");
+// static cl::extrahelp MoreHelp("\nMore help text...");
+
+#include <gtest/gtest.h>
+
+TEST(TestCase, Name) {
+  
+}
 
 int main(int argc, const char **argv) {
   // CommonOptionsParser OptionsParser(argc, argv, MyToolCategory);
