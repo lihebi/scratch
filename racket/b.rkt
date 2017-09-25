ed1d1179c773f867e743d5804073be265b8b1f03
Hebi Li <hebi@hebi.student.iastate.edu>
2017-09-25 13:20:28 -0500

    update in lecture

--- racket/b.rkt
+++ racket/b.rkt
@@ -1,5 +1,7 @@
 ;; define function
-(define square)
+(define square
+  (lambda (x) (* x x)))
+(square 3)
 
 ;; control structure
 (if #t 1 2)
@@ -9,37 +11,83 @@
     (- 1 2))
 
 ;; pair & list
-(car (list 1))
-
-(null? (list))
-
-(list 1 2 3 4 5)
-'(1 2 3 4 5)
-(list 1 2 "hello" 3)
-(car (list 1 2 3))
-(cdr (list 1 2 3))
-(cdr (cdr (list 1 2 3)))
-(cdr (list 1))
 
 (cons 1 2)
-(cons 3 (list 3))
-(cons 1 '())
-(cons 1 #t)
+(cons 2 (cons 1 (cons 2 '())))
+(list 1 2 3)
+'(1 2 3)
+(car (cons 1 2))
+(cdr (cons 1 2))
 
+(car (list 1 2 3))
+(car (cdr (cdr (list 1 2 3))))
+
+(cons 1 2)
+(cons 1 '(2 3))
+'(12 "fna" 3)
 
 (define mylist '((1 2) (3 4)))
 (cons '(5 6) mylist)
 (car (car (cdr mylist)))
 
 
-(define my-cadr)
+(define my-cadr
+  (lambda (lst)
+    (car (cdr lst))))
 (define my-caddr)
 
-(define my-last)
-(define my-append)
-(define my-length)
-(define my-max)
-(define my-reverse)
+(define my-last
+  (lambda (lst)
+    (if (= 1(length lst))
+        (car lst)
+        (my-last (cdr lst)))))
+(my-last '(1 2 3))
+
+(append '(1 2 3) '(4 5 6) '(3 4))
+(define my-append
+  (lambda (a b)
+    (if (null? a)
+        b
+        (cons (car a)
+              (my-append (cdr a) b)))))
+(my-append '(1 2) '(3 4))
+(define my-length
+  (lambda (lst)
+    (if (null? lst)
+        0
+        (+ 1 (my-length (cdr lst))))))
+(my-length '(1 2 3))
+(max 1 2 3)
+(my-max '(-1 -3 -2 -1))
+(define my-max
+  (lambda (lst)
+    (if (null? lst)
+        0
+        (if (> (car lst)
+               (my-max (cdr lst)))
+            (car lst)
+            (my-max (cdr lst))))))
+(reverse '(1 2 3))
+(define my-reverse
+  (lambda (lst)
+    (if (null? lst)
+        '()
+        (append (my-reverse (cdr lst))
+                (list (car lst)))
+        )))
+(my-reverse '(1 2 3))
+(define my-reverse-2
+  (lambda (lst res)
+    (if (null? lst)
+        res
+        (my-reverse-2
+         (cdr lst)
+         (cons (car lst) res)))))
+(define my-wrapper
+  (lambda (lst)
+    (my-reverse-2 lst '())))
+(my-wrapper '(2 3 4))
+(my-reverse-2 '(2 3 4) '())
 
 ;; high order function
 (define arbitrary-arith
@@ -62,6 +110,10 @@
   (lambda (fst snd)
     (lambda (op)
       (if op fst snd))))
+
+(list (list 1 2) '(3 4))
+
+(list (cons 1 2) (cons 3 4))
 
 (pair 1 2)
 
