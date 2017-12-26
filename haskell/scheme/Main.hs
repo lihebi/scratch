829be7f511fd10c0f80cf537d8546972bcd1fa49
Hebi Li <lihebi.com@gmail.com>
2017-12-26 00:17:22 -0600

    finish env

--- haskell/scheme/Main.hs
+++ haskell/scheme/Main.hs
@@ -14,14 +14,13 @@ import System.Environment
 import System.IO
 import Data.IORef
 
-type Env = IORef [(String, IORef LispVal)]
 
 main :: IO ()
 main = do
   args <- getArgs
   case length args of
     0 -> runRepl
-    1 -> evalAndPrint $ args !! 0
+    1 -> runOne $ args !! 0
     otherwise -> putStrLn "Program takes 0 or 1 argument"
 
 flushStr :: String -> IO ()
@@ -29,10 +28,13 @@ flushStr str = putStr str >> hFlush stdout
 
 readPrompt :: String -> IO String
 readPrompt prompt = flushStr prompt >> getLine
-evalString :: String -> IO String
-evalString expr = return $ extractValue $ trapError (liftM show $ readExpr expr >>= eval)
-evalAndPrint :: String -> IO ()
-evalAndPrint expr = evalString expr >>= putStrLn
+
+evalString :: Env -> String -> IO String
+-- evalString expr = return $ extractValue $ trapError (liftM show $ readExpr expr >>= eval)
+evalString env expr = runIOThrows $ liftM show $ (liftThrows $ readExpr expr) >>= eval env
+
+evalAndPrint :: Env -> String -> IO ()
+evalAndPrint env expr = evalString env expr >>= putStrLn
 
 -- naming convention: ends with an underscore: monadic functions that
 -- repeat but do not return a value
@@ -49,11 +51,81 @@ until_ pred prompt action = do
      then return ()
      else action result >> until_ pred prompt action
 
-runRepl :: IO ()
-runRepl = until_ (== "quit") (readPrompt "Lisp>>> ") evalAndPrint
 
-myadd :: Num a => a -> a -> a
-myadd a b = a + b
+runOne :: String -> IO ()
+runOne expr = nullEnv >>= flip evalAndPrint expr
+
+runRepl :: IO ()
+runRepl = nullEnv >>= until_ (== "quit") (readPrompt "Lisp>>> ") . evalAndPrint
+
+
+
+-- Env
+
+type Env = IORef [(String, IORef LispVal)]
+
+nullEnv :: IO Env
+nullEnv = newIORef []
+
+-- Haskell do clause can only deal with one monad.  using one monad
+-- transformer, ErrorT, to combine multiple monads.
+type IOThrowsError = ErrorT LispError IO
+
+liftThrows :: ThrowsError a -> IOThrowsError a
+liftThrows (Left err) = throwError err
+liftThrows (Right val) = return val
+
+runIOThrows :: IOThrowsError String -> IO String
+runIOThrows action = runErrorT (trapError action) >>= return . extractValue
+
+isBound :: Env -> String -> IO Bool
+-- readIORef envRef: get the value stored in the IORef
+-- remember lookup returns a Maybe
+-- use maybe to return False or True
+-- return to lift it into IO monad
+isBound envRef var = readIORef envRef >>= return . maybe False (const True) . lookup var
+
+getVar :: Env -> String -> IOThrowsError LispVal
+-- TODO liftIO lifts the readIORef action into combined monad
+getVar envRef var = do env <- liftIO $ readIORef envRef
+                       -- throwError does not need to be lifted, because it is defined for MonadError
+                       maybe (throwError $ UnboundVar "Getting an unbound variable" var)
+                             (liftIO . readIORef)
+                             (lookup var env)
+setVar :: Env -> String -> LispVal -> IOThrowsError LispVal
+setVar envRef var value = do env <- liftIO $ readIORef envRef
+                             maybe (throwError $ UnboundVar "Setting an unbound variable" var)
+                                   -- writeIORef can set a value to
+                                   -- key.  Because writeIORef is ::
+                                   -- ref -> value, we use flip to
+                                   -- change the order of these two
+                                   -- parameters
+                                   (liftIO . (flip writeIORef value))
+                                   (lookup var env)
+                             -- we are returning the value, the
+                             -- side-effect has already been made
+                             return value
+
+defineVar :: Env -> String -> LispVal -> IOThrowsError LispVal
+defineVar envRef var value = do
+  alreadyDefined <- liftIO $ isBound envRef var
+  if alreadyDefined
+     then setVar envRef var value >> return value
+     else liftIO $ do
+          valueRef <- newIORef value
+          env <- readIORef envRef
+          writeIORef envRef ((var, valueRef) : env)
+          return value
+
+bindVars :: Env -> [(String, LispVal)] -> IO Env
+-- TODO why the newIORef at the end of pipeline
+bindVars envRef bindings = readIORef envRef >>= extendEnv bindings >>= newIORef
+  where extendEnv bindings env = liftM (++ env) (mapM addBinding bindings)
+        addBinding (var, value) = do ref <- newIORef value
+                                     return (var, ref)
+
+
+-- parser
 
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
@@ -192,23 +264,28 @@ unwordsList = unwords . map showVal
 instance Show LispVal where show = showVal
 
 
-eval :: LispVal -> ThrowsError LispVal
-eval val@(String _) = return val
-eval val@(Number _) = return val
-eval val@(Bool _) = return val
-eval (List [Atom "quote", val]) = return val
+eval :: Env -> LispVal -> IOThrowsError LispVal
+eval env val@(String _) = return val
+eval env val@(Number _) = return val
+eval env val@(Bool _) = return val
+eval env (Atom id) = getVar env id
+eval env (List [Atom "quote", val]) = return val
 
-eval (List [Atom "if", pred, conseq, alt]) =
-  do result <- eval pred
+eval env (List [Atom "if", pred, conseq, alt]) =
+  do result <- eval env pred
      case result of
-       Bool False -> eval alt
-       otherwise -> eval conseq
+       Bool False -> eval env alt
+       otherwise -> eval env conseq
 
+eval env (List [Atom "set!", Atom var, form]) =
+  eval env form >>= setVar env var
+eval env (List [Atom "define", Atom var, form]) =
+  eval env form >>= defineVar env var
 -- mapM maps a monadic function over a list
 -- it returns Right [results] on success, Left error on failure
-eval (List (Atom func : args)) = mapM eval args >>= apply func
+eval env (List (Atom func : args)) = mapM (eval env) args >>= liftThrows . apply func
 
-eval badForm = throwError $ BadSpecialForm "Unrecognized special form" badForm
+eval env badForm = throwError $ BadSpecialForm "Unrecognized special form" badForm
 
 apply :: String -> [LispVal] -> ThrowsError LispVal
 -- lookup is a Haskell function that looks up a key inside a list of
