d45a49a15be1a7a705b13ca3cbf023c40ae48fcb
Hebi Li <lihebi.com@gmail.com>
2018-11-19 09:59:07 -0600

    anti-rouge startup

---
+++ deeplearning/anti-rouge/main.py
@@ -0,0 +1,62 @@
+import tensorflow as tf
+import tensorflow_hub as hub
+import numpy as np
+
+def main():
+    """Steps:
+    1. preprocess data:
+      - tokenization
+      - separate article and reference summary
+      - chunk into train and test
+
+    2. data generation: for each reference summary, do the following
+    mutation operations: deletion, insertion, mutation. According to
+    how much are changed, assign a score.
+    
+    3. sentence embedding: embed article and summary into sentence
+    vectors. This is the first layer, the embedding layer. Then, do a
+    padding to get the vector to the same and fixed dimension
+    (e.g. summary 20, article 100). FIXME what to do for very long
+    article? Then, fully connected layer directly to the final result.
+
+    """
+    pass
+
+def sentence_embedding():
+    module_url = "https://tfhub.dev/google/universal-sentence-encoder/2"
+    # Import the Universal Sentence Encoder's TF Hub module
+    embed = hub.Module(module_url)
+
+    # Compute a representation for each message, showing various lengths supported.
+    word = "Elephant"
+    sentence = "I am a sentence for which I would like to get its embedding."
+    paragraph = (
+        "Universal Sentence Encoder embeddings also support short paragraphs. "
+        "There is no hard limit on how long the paragraph is. Roughly, the longer "
+        "the more 'diluted' the embedding will be.")
+    messages = [word, sentence, paragraph]
+
+    # Reduce logging output.
+    tf.logging.set_verbosity(tf.logging.ERROR)
+
+    with tf.Session() as session:
+        session.run([tf.global_variables_initializer(), tf.tables_initializer()])
+        message_embeddings = session.run(embed(messages))
+
+        for i, message_embedding in enumerate(np.array(message_embeddings).tolist()):
+            print("Message: {}".format(messages[i]))
+            print("Embedding size: {}".format(len(message_embedding)))
+            message_embedding_snippet = ", ".join(
+                (str(x) for x in message_embedding[:3]))
+            print("Embedding: [{}, ...]\n".format(message_embedding_snippet))
+
+def test():
+    embed = hub.Module("https://tfhub.dev/google/universal-sentence-encoder/2")
+    embeddings = embed(["The quick brown fox jumps over the lazy dog."])
+    session = tf.Session()
+    print (session.run(embeddings))
+    pass
+
+def main():
+    with tf.device('/cpu:0'):
+        sentence_embedding()
